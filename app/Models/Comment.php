10529664d96a46211a1add252d1496329fc12740
Ricardo CF <ri_ca_fa@hotmail.com>
2020-02-23 11:01:38 -0300

    doing relationship between models classes and db

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'title',
+        'body',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
