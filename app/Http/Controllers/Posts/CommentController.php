a9dbdcd5c57d9d4d96b2250869e920b9457b5a6f
Ricardo CF <ri_ca_fa@hotmail.com>
2020-02-24 09:17:41 -0300

    adding queue to send mail notification for user.. using mailtrap.io to test mail

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Posts;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCommentFormRequest;
+use App\Notifications\PostCommented;
 
 class CommentController extends Controller
 {
@@ -13,6 +13,9 @@ class CommentController extends Controller
         //dd($request->all());
         $comment = $request->user()->comments()->create($request->all());
         //dd($comment);
+
+        $author = $comment->post->user;
+        $author->notify(new PostCommented($comment));
 
         return redirect()
             ->route('posts.show', $comment->post->id)
---
+++ app/Notifications/PostCommented.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class PostCommented extends Notification
+{
+    use Queueable;
+    private $comment;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject("Novo Comentário em {$this->comment->post->title}")
+                    ->line($this->comment->body)
+                    ->action('Acessar Post', route('posts.show', $this->comment->post->id))
+                    ->line('Obrigado por utilizar nosso sistema!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
